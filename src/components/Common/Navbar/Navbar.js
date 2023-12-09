bf8cf674c314876a125e3e2ed9f18681c133817b
simona.tocanie <simona.tocanie@yahoo.com>
2023-12-10 01:56:18 +0200

    fixed issue related to active class and small changes

--- src/components/Common/Navbar/Navbar.js
+++ src/components/Common/Navbar/Navbar.js
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
-import './Navbar.css'
+import styles from './Navbar.module.css'
+import { useState } from "react";
 
 const Navbar = () => {
+    const [activeLink, setActiveLink] = useState('home')
+
+    const checkActiveClass = (link) => {
+        if (activeLink === link) {
+            return styles.active;
+        }
+
+        return '';
+    }
+
     return (
         <ul>
-            <li><Link className="active" to='/'>Home</Link></li>
-            <li><Link to='/calculator'>Calculator</Link></li>
+            <li><Link className={checkActiveClass('home')} to='/' onClick={() => setActiveLink('home')}>Home</Link></li>
+            <li><Link className={checkActiveClass('calculator')} to='/calculator' onClick={() => setActiveLink('calculator')}>Calculator</Link></li>
         </ul>
     );
 }
